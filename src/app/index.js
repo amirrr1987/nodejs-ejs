6e13a592393fa2ddd32342d4b0e88012607ffca4
Amir Maghami <amaghami@haco.dc>
2022-07-02 14:10:33 +0430

    06: use mongoose

--- src/app/index.js
+++ src/app/index.js
@@ -2,17 +2,27 @@ const htpp = require('http');
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const path = require('path');
+const mongoose = require('mongoose');
 const app = express();
 
 module.exports = class Application {
     constructor() {
         this.serverConfig();
+        this.databaseConfig();
         this.setConfig();
         this.setRoutes();
     };
     serverConfig() {
         app.listen(5000, () => {
             console.log('Server is running on port: ' + 5000);
+        });
+    };
+    async databaseConfig () {
+        await mongoose.connect('mongodb://localhost/my-app', {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            // useFindAndModify: false,
+            // useCreateIndex: true,
         });
     };
     setConfig() {
